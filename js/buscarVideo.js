77a0d3293b672da1b26f7352e27ed70ba3cd76c5
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-05 22:08:23 -0300

    ...

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,20 +1,21 @@
 import { conectaApi } from "./conectaApi.js";
 import constroiCard from "./mostrarVideos.js";
 
-async function buscaVideo(evento) {
+async function buscarVideo(evento) {
     evento.preventDefault();
-    const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    const lista = document.querySelector('[data-lista]')
+    const lista = document.querySelector("[data-lista]");
 
-    while(lista.firstChild) {
-        lista.removeChild(lista.firstChild)
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
     }
 
-    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
-const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]');
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
 
-botaoDePesquisa.addEventListener('click', evento => buscaVideo(evento))
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento))
